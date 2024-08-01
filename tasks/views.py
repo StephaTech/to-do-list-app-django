0dfd83472d965bbc6422a9f142d672c803c7fc57
StephaTech <stephatech123@gmail.com>
2024-08-01 22:18:51 +0100

    first commit

---
+++ tasks/views.py
@@ -0,0 +1,50 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from .models import * #adding the import model
+from .forms import *
+
+# Create your views here.
+
+# TEMPLATE_DIRS = (
+#     'os.path.join(BASE_DIR, "templates"),'
+# )
+def index(request):
+    tasks =Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    
+
+    context = {'tasks' : tasks, 'form':form}
+    return render(request, 'tasks/list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+         form = TaskForm(request.POST, instance=task)
+         if form.is_valid():
+            form.save()
+         return redirect('/')
+    
+    context = {'form':form}
+
+    return render(request, 'tasks/update_task.html', context)
+
+def deleteTask(request,pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item' :item}
+    return render(request, 'tasks/delete.html', context)
